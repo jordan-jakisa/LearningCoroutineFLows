8032f0acef92f9e9dd140f43580e2d0979f3ae3c
Jordan <jordan.jakisa@gmail.com>
2023-02-27 21:18:30 +0300

    Terminal flow operators

--- app/src/main/java/com/empire/kotlinflowslearning/MainViewModel.kt
+++ app/src/main/java/com/empire/kotlinflowslearning/MainViewModel.kt
@@ -3,32 +3,56 @@ package com.empire.kotlinflowslearning
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     init {
         collectFlow()
     }
 
     //Cold flow: will not emit any values if there are no collectors attached.
-    val countDownFlow = flow<Int>{
+    val countDownFlow = flow<Int> {
         val startingValue = 10
         var currentValue = startingValue
         emit(currentValue)
-        while (currentValue > 0){
+        while (currentValue > 0) {
             delay(1000L)
             currentValue--
             emit(currentValue)
         }
     }
 
-    private fun collectFlow(){
+    private fun collectFlow() {
         viewModelScope.launch {
-            countDownFlow.collect{ time ->
-                println("Current time is: $time")
+            countDownFlow
+                //terminal flow operators
+                .filter { time ->
+                    time % 2 == 0
+                    //filter flow operator, filters a list basing on a defined criteria.
+                }
+                .map { time ->
+                    time * time
+                    //map: operator takes the time value and multiplies it by itself
+                }.onEach { time ->
+                    println(time)
+                    // onEach operator is similar to the collect operator but it doesn't conclude the flow
+                }
+                .collect { time ->
+                    println("Current time is: $time")
+                }
+
+            //Terminal flow operator: Counts the number of items matching the condition
+            val count = countDownFlow.count {
+                it % 2 == 0
             }
+            println("The count is $count")
         }
+
+        //does the same role as the above block of code without the flow operators
+        countDownFlow.onEach {time ->
+            println(time)
+        }.launchIn(viewModelScope)
     }
 }
